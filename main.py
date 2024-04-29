23f067a030c8f392b556ecfc36983c26bfe45c36
TakumaIkeda <i.taku0415@gmail.com>
2024-04-30 02:20:16 +0900

    Update main.py with shake function and adjust animation interval

--- main.py
+++ main.py
@@ -1,31 +1,51 @@
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
+from numpy.linalg import norm
 
 ims = []
+
+d01 = 1.0
+d12 = 1.0
 
 r0 = np.array([0.0, 3.0])
 r1 = np.array([0.0, 2.0])
 r2 = np.array([0.0, 1.0])
 
 p1 = np.array([0.0, 0.0])
-p2 = np.array([-10.0, 0.0])
+p2 = np.array([-1.0, 0.0])
 
 m1 = 1.0
-m2 = 1.0
+m2 = 2.0
 
 step = 0
-dt = 0.1
+dt = 0.01
 g = 9.8
 
 fig = plt.figure()
 
-def plot(r1, r2, ims):
-    im = plt.scatter([3, r1[0], r2[0]], [3, r1[1], r2[1]])
+def plot(r0, r1, r2, ims):
+    im = plt.scatter([r0[0], r1[0], r2[0]], [r0[1], r1[1], r2[1]], c=['red', 'red', 'red'])
     ims.append([im])
     return ims
 
-plot(r1, r2, ims)
+plot(r0, r1, r2, ims)
+
+def shake(r1_prev, r2_prev, r1, r2):
+  gamma01 = 1.0
+  gamma12 = 1.0
+  while abs(gamma01) > 1e-6 or abs(gamma12) > 1e-6:
+    gamma01 = 1 / norm(r1_prev) * (d01 - (norm(r1) + norm(r0)))
+    r1 += gamma01 * r1_prev
+    a = (1 / m1 + 1 / m2) * (norm(r1_prev) - norm(r2_prev))
+    b = 2 * (1 / m1 + 1 / m2) * (norm(r1_prev) - norm(r2_prev)) * (norm(r1) - norm(r2))
+    c = (norm(r1) - norm(r2)) ** 2 - d12 ** 2
+    gamma12 = (-b + np.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+    r1 += gamma12 * (r1_prev - r2_prev) / m1
+    r2 += gamma12 * (r2_prev - r1_prev) / m2
+
+    print(f"shake composed r1: {r1}, r2: {r2}, gamma01: {gamma01}, gamma12: {gamma12}")
+  return r1, r2, gamma01, gamma12
 
 while step < 1000:
   V1 = m1 * g * r1[1]
@@ -34,20 +54,34 @@ while step < 1000:
   F1 = np.array([0.0, -m1 * g])
   F2 = np.array([0.0, -m2 * g])
 
+  # calculate p(t+dt/2)
   p1 += 0.5 * dt * F1
   p2 += 0.5 * dt * F2
+
+  # calculate r(t+dt)
+  r1_prev = r1.copy()
+  r2_prev = r2.copy()
 
   r1 += dt * p1 / m1
   r2 += dt * p2 / m2
 
+  # print(r1, r2)
+
+  # gamma01 = 1 / r1 * (d01 - (r1 + r0))
+  # gamma12 = ((r2 - r1).linalg.norm() - d12) / (1 / m1 + 1 / m2) * (r1_prev - r2_prev).linalg.norm()
+  r1, r2, gamma01, gamma12 = shake(r1_prev, r2_prev, r1, r2)
+
   F1 = np.array([0.0, -m1 * g])
   F2 = np.array([0.0, -m2 * g])
 
+  # calculate p(t+dt)
   p1 += 0.5 * dt * F1
   p2 += 0.5 * dt * F2
 
-  step += 1
-  ims = plot(r1, r2, ims)
 
-ani = animation.ArtistAnimation(fig, ims, interval=1)
+  step += 1
+  ims = plot(r0, r1, r2, ims)
+  break
+
+ani = animation.ArtistAnimation(fig, ims, interval=1000)
 ani.save('test.gif')
